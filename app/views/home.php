7d3248b5c1843a7efbf1e671f4de695b431ecd44
GMDorneles <gabrielly.maldonadoo@gmail.com>
2021-02-24 08:52:10 -0400

    Fotos de colaboradores

--- app/views/home.php
+++ app/views/home.php
@@ -6,14 +6,29 @@
 	</div>
 <video autoplay loop muted playsinline src="app\images\Hikvision.mp4"></video>
 </section>
-<div class="fundo-branco">
-	<div class="texto">
-		<h2>Quem Somos:<h2>
-	</div>
-	
-
-	<div class="imagem">
-	<img   width="400 px" src="app/images/Loja.png">
-	</div>
+<div class="container">
+		<img class="Empresa"  src="app\images\lojaespaço.jpg">	
+		<div class="texto">
+			<h2>Quem Somos<h2>
+		</div>
+		<div class="representantes">
+			<img class="img1" src="app\images\pic01.jpg">
+			<img class="img1" src="app\images\pic02.jpg">
+			<img class="img1" src="app\images\pic03.jpg">
+			<img class="img1" src="app\images\pic01.jpg">
+		</div>
+		<div class="representantes-texto">
+		<p>Representante 1 </p>
+		<p>Representante 2 </p>
+		<p>Representante 3 </p>
+		<p>Representante 4 </p>
+		</div>
+		<div class="representantes-desc">
+		<p>Sobre 1 </p>
+		<p>Sobre 2 </p>
+		<p>Sobre 3 </p>
+		<p>Sobre 4 </p>
+		</div>
 	
 </div>
+	
